191eb3c50e079cbf5b2d661d43a651388d52457e
Mohammed Sharaf <153912332+MrMYMSharaf@users.noreply.github.com>
2024-03-02 19:58:43 +0530

    C++ OpenGL Project
    C++ OpenGL Project

---
+++ Beginner Level Projects/05 C++OpenGL/Project1.cpp
@@ -0,0 +1,33 @@
+#include <windows.h>
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+        glColor3f(1.0, 0.0, 0.0);
+        glVertex2f(0.0, 1.0);
+        glColor3f(0.0, 1.0, 0.0);
+        glVertex2f(-1.0, -1.0);
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex2f(1.0, -1.0);
+    glEnd();
+    glFlush();
+}
+
+void init() {
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Simple OpenGL Program");
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
+
